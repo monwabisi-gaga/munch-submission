f5b5e8a957bd2b3b815299f771c152a32d02b06e
Monwabisi Gaga <40353534+monwabisi-gaga@users.noreply.github.com>
2025-09-07 09:36:28 +0000

    add config (most of these were generated from commands and copied and pasted from the website documentation)

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+export default {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapping: {
+    '^@shared/(.*)$': '<rootDir>/shared/$1',
+    '^../server/(.*)$': '<rootDir>/server/$1',
+    '^../shared/(.*)$': '<rootDir>/shared/$1'
+  },
+  transform: {
+    '^.+\\.ts$': ['ts-jest', {
+      useESM: true
+    }]
+  },
+  collectCoverageFrom: [
+    'server/**/*.ts',
+    'shared/**/*.ts',
+    '!server/vite.ts',
+    '!server/index.ts',
+    '!**/node_modules/**',
+    '!**/dist/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  },
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts']
+};
